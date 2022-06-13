b68caa7892220d26a183261c245f33691255f6b8
Ryu-nakay-from_MBA <70783066+Ryu-nakay@users.noreply.github.com>
2022-06-13 09:36:28 +0900

    Initial Commit

---
+++ RealmPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealmPractice
+//
+//  Created by 中山龍 on 2022/06/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
